383b539623a600a6380510f80dafbf1c209c468f
Mohd Sahil <mohdsahilsiddiqui36@gmail.com>
2024-08-24 00:00:47 +0530

    first commit-structure,navbar

---
+++ components/Navbar/Navbar.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+
+export default function Navbar(){
+    return(
+        <div>
+            <nav>
+                <ul className='flex justify-between md:justify-evenly mx-3'>
+                    <li className='text-xs md:text-base'>About</li>
+                    <li className='text-xs md:text-base mx-3'>Tech Stack</li>
+                    <li className='text-xs md:text-base'>Work</li>
+                    <li className='text-xs md:text-base mx-3'>Projects</li>
+                    <li className='text-xs md:text-base'>Contact</li>
+                </ul>
+            </nav>
+        </div>
+    )
+}
